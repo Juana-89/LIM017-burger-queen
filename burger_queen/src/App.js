835b169a3be30c19160d295b82fca71ba2277d58
unknown <Juana-89>
2022-06-20 00:31:33 -0500

    Adding login view

--- burger_queen/src/App.js
+++ burger_queen/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import '../src/stylesheets/Welcome.css';
+import '../src/stylesheets/Login.css';
 import { Welcome } from './components/Welcome';
+//import { Login } from './components/Login';
 
 function App() {
   return (
     <div className="App">
     <div className="container-main">
     <Welcome />
+    {/* <Login /> */}
     </div>
     </div>
   );
