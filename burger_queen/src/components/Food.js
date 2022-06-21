33b4dd4590caf1101ec5ea6cd556e77da947353f
thaliaroman <thaly31955@gmail.com>
2022-06-20 21:32:37 -0500

    iniciando con router

---
+++ burger_queen/src/components/Food.js
@@ -0,0 +1,11 @@
+import React from 'react';
+export function Food() {
+    return (
+        <div className='container-options'>
+            <div className='btn-options'>
+            <input type="button" className="btn-food" value="DESAYUNO"/> <br/> <br />
+            <input type="button" className="btn-food" value="ALMUERZO Y CENA"/>
+            </div>
+        </div>
+    )
+}
