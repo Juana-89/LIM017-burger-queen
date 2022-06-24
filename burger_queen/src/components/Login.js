0ee8a98e73edc03cd2539b3d95d9237024570ba0
unknown <Juana-89>
2022-06-24 09:22:43 -0500

    tweaks in the login

--- burger_queen/src/components/Login.js
+++ burger_queen/src/components/Login.js
@@ -45,7 +45,8 @@ export function Login() {
 
  <div className="container-login">
 
-    <div id="carouselBurger" className="carousel carousel-dark slide" data-bs-ride="carousel">
+
+<div id="carouselBurger" className="carousel carousel-dark slide" data-bs-ride="carousel">
     <div className="carousel-indicators">
     <button type="button" data-bs-target="#carouselBurger" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselBurger" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -76,8 +77,6 @@ export function Login() {
     <span className="visually-hidden">Next</span>
     </button>
 </div>
-
-
 
 
 
