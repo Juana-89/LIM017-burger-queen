bcd4c74e88012c02fe9eca2dd9686d133dec1018
thaliaroman <thaly31955@gmail.com>
2022-07-07 11:42:12 -0500

    correcting errors of App.js

--- burger_queen/src/App.js
+++ burger_queen/src/App.js
@@ -5,22 +5,17 @@ import { Login } from './components/Login';
 import { WaiterMainView } from './components/WaiterView/WaiterMainView';
 import { Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/authContext';
-import  Main  from '../src/components/WaiterView1/Main';
-import Cart from '../src/components/WaiterView1/Cart';
-import { CartProvider } from 'react-use-cart'
 function App() {
   return (
     <div className="App">
     <div className="container-main">
     <AuthProvider>
     <Routes>
-    <CartProvider>
-    <Main />
-      <Cart />
+    
       <Route path="/" element={<Welcome/>}/>
       <Route path="/login" element={<Login/>}/>
       <Route path="/waiter" element={<WaiterMainView/>}/>
-      </CartProvider>
+  
     </Routes>
     </AuthProvider>
 
