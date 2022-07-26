ce2a0d592b874d8b8a4d239f172f7d4eddbaba23
Juana Llanje <juana.lc@hotmail.es>
2022-07-25 19:02:04 -0500

    Updates

---
+++ precache-manifest.a86bda33f99d4112ed906d06a6751c1e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "74d98b049b0c312f2864",
+    "url": "/LIM017-burger-queen/static/css/main.2e25eb93.chunk.css"
+  },
+  {
+    "revision": "74d98b049b0c312f2864",
+    "url": "/LIM017-burger-queen/static/js/main.74d98b04.chunk.js"
+  },
+  {
+    "revision": "af97aced6fe0babb3136",
+    "url": "/LIM017-burger-queen/static/css/1.3729cda0.chunk.css"
+  },
+  {
+    "revision": "af97aced6fe0babb3136",
+    "url": "/LIM017-burger-queen/static/js/1.af97aced.chunk.js"
+  },
+  {
+    "revision": "d28c629207b72b66cce0",
+    "url": "/LIM017-burger-queen/static/js/runtime~main.d28c6292.js"
+  },
+  {
+    "revision": "32ce6b06d569709914cb41fc84c54fda",
+    "url": "/LIM017-burger-queen/static/media/font1.32ce6b06.png"
+  },
+  {
+    "revision": "063db6f83bcf8b41a92ed137a684bbb9",
+    "url": "/LIM017-burger-queen/static/media/burger_load.063db6f8.png"
+  },
+  {
+    "revision": "0730db19981187b5d5d7e93578e6dd48",
+    "url": "/LIM017-burger-queen/static/media/burger1.0730db19.jpg"
+  },
+  {
+    "revision": "b7a59245c123d15e71da8f1572fc737e",
+    "url": "/LIM017-burger-queen/static/media/burger2.b7a59245.jpg"
+  },
+  {
+    "revision": "179cc5a4809f3e70d817529318ed06d4",
+    "url": "/LIM017-burger-queen/static/media/burger3.179cc5a4.jpg"
+  },
+  {
+    "revision": "1979e8e644c51618985015a0c46252d1",
+    "url": "/LIM017-burger-queen/static/media/font2.1979e8e6.png"
+  },
+  {
+    "revision": "eb01963d3dca838a14111c9c80f9bc22",
+    "url": "/LIM017-burger-queen/static/media/fondo1.eb01963d.png"
+  },
+  {
+    "revision": "4f51db28ce3782b074b6e75a10c0e390",
+    "url": "/LIM017-burger-queen/index.html"
+  }
+];
