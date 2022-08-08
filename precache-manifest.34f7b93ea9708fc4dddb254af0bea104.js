d1bc1423938f280ef46a7a6a4a1b0ff43d7e0a59
Juana Llanje <juana.lc@hotmail.es>
2022-08-07 19:13:16 -0500

    Updates

---
+++ precache-manifest.34f7b93ea9708fc4dddb254af0bea104.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf95c234e6a0569f110d",
+    "url": "/LIM017-burger-queen/static/css/main.feea9510.chunk.css"
+  },
+  {
+    "revision": "cf95c234e6a0569f110d",
+    "url": "/LIM017-burger-queen/static/js/main.cf95c234.chunk.js"
+  },
+  {
+    "revision": "b027bbf33bcfc4af2b52",
+    "url": "/LIM017-burger-queen/static/css/1.8a617aa8.chunk.css"
+  },
+  {
+    "revision": "b027bbf33bcfc4af2b52",
+    "url": "/LIM017-burger-queen/static/js/1.b027bbf3.chunk.js"
+  },
+  {
+    "revision": "d28c629207b72b66cce0",
+    "url": "/LIM017-burger-queen/static/js/runtime~main.d28c6292.js"
+  },
+  {
+    "revision": "32ce6b06d569709914cb41fc84c54fda",
+    "url": "/LIM017-burger-queen/static/media/font1.32ce6b06.png"
+  },
+  {
+    "revision": "063db6f83bcf8b41a92ed137a684bbb9",
+    "url": "/LIM017-burger-queen/static/media/burger_load.063db6f8.png"
+  },
+  {
+    "revision": "0730db19981187b5d5d7e93578e6dd48",
+    "url": "/LIM017-burger-queen/static/media/burger1.0730db19.jpg"
+  },
+  {
+    "revision": "b7a59245c123d15e71da8f1572fc737e",
+    "url": "/LIM017-burger-queen/static/media/burger2.b7a59245.jpg"
+  },
+  {
+    "revision": "179cc5a4809f3e70d817529318ed06d4",
+    "url": "/LIM017-burger-queen/static/media/burger3.179cc5a4.jpg"
+  },
+  {
+    "revision": "1979e8e644c51618985015a0c46252d1",
+    "url": "/LIM017-burger-queen/static/media/font2.1979e8e6.png"
+  },
+  {
+    "revision": "eb01963d3dca838a14111c9c80f9bc22",
+    "url": "/LIM017-burger-queen/static/media/fondo1.eb01963d.png"
+  },
+  {
+    "revision": "2112cfdc325ad8bc9f67944e83aa6f35",
+    "url": "/LIM017-burger-queen/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Juana-89/LIM017-burger-queen/burger_queen/precache-manifest.b6d64109d8248b27cc10a041a0afa914.js"
+  "/LIM017-burger-queen/precache-manifest.34f7b93ea9708fc4dddb254af0bea104.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Juana-89/LIM017-burger-queen/burger_queen/index.html", {
+workbox.routing.registerNavigationRoute("/LIM017-burger-queen/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
